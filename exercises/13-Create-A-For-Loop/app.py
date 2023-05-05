c44847e12fe123579587603f983e47da546d4944
Miguel Ramirez <121323094+mramirez092@users.noreply.github.com>
2023-05-05 19:12:44 +0000

    OK

--- exercises/13-Create-A-For-Loop/app.py
+++ exercises/13-Create-A-For-Loop/app.py
@@ -1,4 +1,8 @@
 def standards_maker():
     #your code here
+    txt = "Escribiré preguntas si estoy atascado"
+    for i in range(300):
+        print(txt)
 
 #remember to call the function outside (here)
+standards_maker()
